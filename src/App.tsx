5678248451f685494a073beade3b86a49889c417
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-03-13 17:03:50 +0200

    Set up React Query, Material UI, Styled Components & Fetch Store API

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
 
-function App() {
-  return (
-    <div></div>
-  );
-}
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+
+import { Wrapper } from './App.styles';
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType> => {
+  return await (await fetch('https://fakestoreapi.com/products')).json();
+};
+
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
